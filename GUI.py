f650c12ba7d43ffc172bfd9d7b8795a1c38c69c4
Hanna Nabil <hananabil@live.com>
2019-03-16 20:00:37 +0200

    finished 3 button new load save

--- GUI.py
+++ GUI.py
@@ -8,45 +8,6 @@ from PyQt4.QtGui import *
 #from PySide import QtCore, QtGui
 import sys, os
 
-pins_dict={
-    
-            "Pin_0":["Output","High"],     
-            "Pin_1":["Output","High"],
-            "Pin_2":["Output","High"],
-            "Pin_3":["Output","High"],
-            "Pin_4":["Output","High"],
-            "Pin_5":["Output","High"],
-            "Pin_6":["Output","High"],
-            "Pin_7":["Output","High"],
-
-            "Pin_8":["Output","High"],
-            "Pin_9":["Output","High"],
-            "Pin_10":["Output","High"],
-            "Pin_11":["Output","High"],
-            "Pin_12":["Output","High"],
-            "Pin_13":["Output","High"],
-            "Pin_14":["Output","High"],
-            "Pin_15":["Output","High"],
-
-            "Pin_16":["Output","High"],
-            "Pin_17":["Output","High"],
-            "Pin_18":["Output","High"],
-            "Pin_19":["Output","High"],
-            "Pin_20":["Output","High"],
-            "Pin_21":["Output","High"],
-            "Pin_22":["Output","High"],
-            "Pin_23":["Output","High"],
-
-            "Pin_24":["Output","High"],
-            "Pin_25":["Output","High"],
-            "Pin_26":["Output","High"],
-            "Pin_27":["Output","High"],
-            "Pin_28":["Output","High"],
-            "Pin_29":["Output","High"],
-            "Pin_30":["Output","High"],
-            "Pin_31":["Output","High"]
-        };
-
 class Main(QMainWindow):
     def __init__(self, parent=None):
         super(Main, self).__init__()
@@ -54,54 +15,92 @@ class Main(QMainWindow):
         pixmap = QPixmap("sobhy.jpg")
         self.label_image.setPixmap(pixmap)
         self.label_image.setScaledContents(True)
-        self.pushButton_SaveCurrent.clicked.connect(self.SavePinsConfiguration)
+        self.pushButton_SaveCurrent.clicked.connect(self.SaveCurrentPinsConfigToDict)
+        self.pushButton_new.clicked.connect(self.NewPinsConfiguration)
+        self.pushButton_load.clicked.connect(self.LoadPinsConfigurationFromFile)
+        self.pushButton_save.clicked.connect(self.SavePinsConfigurationToFile)
         self.pushButton_Browse.clicked.connect(self.browse)
         self.pushButton_FinalSave.setStyleSheet("background-color: green")  ## This is to change Button Color
         self.checkboxesPins=[
-            self.checkBox,self.checkBox_2,self.checkBox_3,self.checkBox_4,self.checkBox_5,self.checkBox_6,self.checkBox_7 ,self.checkBox_8,\
+            self.checkBox_0,self.checkBox_1,self.checkBox_2,self.checkBox_3,self.checkBox_4,self.checkBox_5,self.checkBox_6,self.checkBox_7 ,self.checkBox_8,\
             self.checkBox_9,self.checkBox_10,self.checkBox_11,self.checkBox_12,self.checkBox_13,self.checkBox_14,self.checkBox_15,self.checkBox_16, \
             self.checkBox_17, self.checkBox_18, self.checkBox_19,self.checkBox_20, self.checkBox_21,self.checkBox_22,self.checkBox_23,self.checkBox_24,\
-            self.checkBox_25, self.checkBox_26,self.checkBox_27, self.checkBox_28,self.checkBox_29,self.checkBox_30,self.checkBox_31,self.checkBox_32]
+            self.checkBox_25, self.checkBox_26,self.checkBox_27, self.checkBox_28,self.checkBox_29,self.checkBox_30,self.checkBox_31]
+        self.labelsPins=[
+            self.label_0,self.label_1,self.label_2,self.label_3,self.label_4,self.label_5,self.label_6,self.label_7, \
+            self.label_8, self.label_9, self.label_10, self.label_11, self.label_12, self.label_13, self.label_14,self.label_15, \
+            self.label_16, self.label_17, self.label_18, self.label_19, self.label_20, self.label_21, self.label_22,self.label_23, \
+            self.label_24, self.label_25, self.label_26, self.label_27, self.label_28, self.label_29, self.label_30,self.label_31
+            ]
+        self.pins_dict_init = {
+            "0": "not configured", "1": "not configured", "2": "not configured", "3": "not configured",
+            "4": "not configured", "5": "not configured", "6": "not configured", "7": "not configured",
+            "8": "not configured", "9": "not configured", "10": "not configured", "11": "not configured",
+            "12": "not configured", "13": "not configured", "14": "not configured", "15": "not configured",
+            "16": "not configured", "17": "not configured", "18": "not configured", "19": "not configured",
+            "20": "not configured", "21": "not configured", "22": "not configured", "23": "not configured",
+            "24": "not configured", "25": "not configured", "26": "not configured", "27": "not configured",
+            "28": "not configured", "29": "not configured", "30": "not configured", "31": "not configured"
+        }
+        self.pins_dict =self.pins_dict_init
+    def apply_pinsLabels(self):
+        for index in range (31):
+            state =self.pins_dict[str(index)]
+            self.labelsPins[index].setText(state)
+            if state == "not configured":  # selected but not configured
+                self.labelsPins[index].setStyleSheet('color: red')
+            else:
+                self.labelsPins[index].setStyleSheet('color: green')
 
-    def get_SelectedPins(self):
-        selectedPins = [i for i in self.checkboxesPins if i.isChecked()]
-        return selectedPins
 
     def browse(self):
         self.path = QtGui.QFileDialog.getExistingDirectory(self, "Select Folder", "")#return the path of the selected folder
         self.Output_Folder_Path_LineEdit.setText(self.path)
 
-    #def SavePinsConfiguration(self):
-    #     self.selectedPins=self.get_SelectedPins()
+    def SaveCurrentPinsConfigToDict(self):
+        if self.Input_button.isChecked():
+            if self.Pull_Up_button.isChecked():
+                state = "Input PullUp"
+            elif self.Pull_Imp_button.isChecked():
+                state = "Input PullImp"
+        elif self.Output_button.isChecked():
+            if self.High_button.isChecked():
+                state = "Output High"
+            elif self.Low_button.isChecked():
+                state = "Output Low"
+        else:
+            state = "not configured"
 
-    def SavePinsConfiguration(self):
-        type(self.groupBox_Pins)
-        print(self.groupBox_Pins)
-        selected_pins_list=[]
-        
-        if self.pushButton_SaveCurrent.isChecked():
-        #self.checkbox_1.isClicked()
-            for checkbox in self.groupBox_Pins:
-                if checkbox.isChecked():
-                    selected_pins_list.append(checkbox.text())
+        for index in range(31):
+            if self.checkboxesPins[index].isChecked():
+                if state =="not configured": # selected but not configured
+                    self.pins_dict[str(index)] = state
+                else :
+                    self.pins_dict[str(index)]=state
+                self.checkboxesPins[index].setChecked(False)
+        self.apply_pinsLabels()
 
+
+    def NewPinsConfiguration(self):
+        self.pins_dict = self.pins_dict_init
+        for i in self.checkboxesPins :
+            i.setChecked(False)
+        self.apply_pinsLabels()
+
+    def LoadPinsConfigurationFromFile(self):
+        filepath = QtGui.QFileDialog.getOpenFileName(self, 'Single File', "", '*.txt')
+        self.pins_dict = eval(open(filepath, 'r').read())
+        if not self.pins_dict:
+            print ("File is Empty")
+        else:
+            self.apply_pinsLabels()
                     
-            for pin in selected_pins_list:
-                if pin in pins_dict: #not sure if right syntax
-                    if self.Input_button.isChecked():
-                        pins_dict[pin][0]="Input"
-                        if self.Pull_Up_button.isChecked():
-                            pins_dict[pin][1]="Pull Up"
-                        elif self.Pull_Imp_button.isChecked():
-                            pins_dict[pin][1]="Pull Down"    
 
-
-                    elif self.Output_button.isChecked():
-                        pins_dict[pin][0]="Output"
-                        if self.High_button.isChecked():
-                            pins_dict[pin][1]="High"
-                        elif self.Low_button.isChecked():
-                            pins_dict[pin][1]="Low"  
+    def SavePinsConfigurationToFile(self):
+        filepath = QtGui.QFileDialog.getSaveFileName(self, 'Save Point', "", '*.txt')
+        file = open(filepath ,'w')
+        file.write(str(self.pins_dict))
+        file.close()
 
     def GenerateFun(self):
         outputFolder = self.Output_Folder_Path_LineEdit.text()
@@ -113,16 +112,14 @@ class Main(QMainWindow):
         pin_mode=""
         index=0
 
-        for pin in pins_dict:
+        for pin in self.pins_dict:
             pin_mode=pin[1]
             DIO_File_handler.write(r'#define DIO_u8_'+pin+'PIN0_Mode      '+pin_mode)
             MFIC_File_handler.write(r'#define   '+pin+index)
             index+=1
       
         DIO_File_handler.close()
-        MFIC_File_handler.close()    
-
-   
+        MFIC_File_handler.close()
     def resource_path(self, relative_path):#used in .exe extraction
         """ Get absolute path to resource, works for dev and for PyInstaller """
         try:
@@ -136,7 +133,6 @@ class Main(QMainWindow):
         #
 
 if __name__ == '__main__':
-
     app = QtGui.QApplication(sys.argv)
     app.setWindowIcon(QtGui.QIcon('mohsen.jpg'))
     main = Main()
